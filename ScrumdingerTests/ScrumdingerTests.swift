d5c7c618c41f4bf93c3450ad95f8cd3bc93dc01c
freddyliu2024 <160204550+freddyliu2024@users.noreply.github.com>
2025-02-06 09:58:45 +0800

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by 劉宣甫 on 2025/2/6.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
